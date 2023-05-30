ee052fb36d1e97209b1ea0f0a49c68a36ad213c5
Ashwini <ashwinilakshmanan1@gmail.com>
2023-05-30 11:16:14 +0530

    created the delete method

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,17 @@
 import React from "react";
 import { Button, Table } from "react-bootstrap";
 import Employees from "./Employees";
+import {Link,useNavigate} from 'react-router-dom'
 
 function Home() {
+    let navigate = useNavigate();
+    const handleDelete=(id)=>{
+        var index = Employees.map((item)=>{
+            return item.id;
+        }).indexOf(id)
+        Employees.splice(index,1);
+        navigate('/')
+    }
   return (
     <>
       <div style={{ margin: "10rem" }}>
@@ -28,7 +37,7 @@ function Home() {
                             <td>
                                 <Button onClick = {()=>alert(item.id)}>Edit</Button>
                                 &nbsp;
-                                <Button onClick = {()=>alert(item.id)}>Delete</Button>
+                                <Button onClick = {()=>handleDelete(item.id)}>Delete</Button>
                             </td>
                         </tr>
                     )
